6c8d70308f0076817e5fa7098177e7de78e11a56
Iago V. Rocha <iagovieira3562@gmail.com>
2025-08-06 22:54:55 -0300

    refactor: remoção dos arquivos antigos

--- backend/app/api/auth_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
-
-@auth_router.get("/")
-def post_infos():
-    return {
-        "mensagem": "Recursos enviados"
-    }
