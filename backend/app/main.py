59617772ee8e764a7ee6a426dee0aa65ae1d07fb
Iago V. Rocha <iagovieira3562@gmail.com>
2025-08-06 22:10:33 -0300

    refactor(main.py) Utilização de 'importações relativas' para melhor visibilidade, adicionada informações a documentação da API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .routers import auth, airfoil_data, upload_imagem, upload_canvas
 
-app = FastAPI()
 
-from api.auth_routes import auth_router
-from api.upload_imagem import upload_imagem_router
-from api.upload_canvas import upload_canvas_router
-from api.input_datas import input_data_router
+app = FastAPI(
+    title="Airfoil Optimizer API",
+    description="""
+        This API provides endpoints for managing airfoil optimization tasks,
+        including authentication, image uploads, canvas uploads, and input data management.
+        
+        It is designed to work with a frontend application for airfoil optimization.
+    """,
+    version="1.0.0",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0",
+    },
+)
 
-app.include_router(auth_router)
-app.include_router(upload_imagem_router)
-app.include_router(upload_canvas_router)
-app.include_router(input_data_router)
+app.include_router(auth.auth_router, tags=["auth"])
+app.include_router(airfoil_data.airfoil_router, tags=["airfoil-data"])
+app.include_router(upload_imagem.upload_imagem_router, tags=["upload-imagem"])
+app.include_router(upload_canvas.upload_canvas_router, tags=["upload-canvas"])
 
 app.add_middleware(
     CORSMiddleware,
