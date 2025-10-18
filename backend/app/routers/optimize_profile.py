ca7706d7dc31502819f5a4f1af36db26e2a99257
Samirborges <alessandro.samir@souunit.com.br>
2025-10-18 16:05:16 -0300

    Adicionando integração com N8N (URL DE TESTE!)

--- backend/app/routers/optimize_profile.py
+++ backend/app/routers/optimize_profile.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from ..services.airfoil_service import build_payload
+import requests
 
 optimize_router = APIRouter(prefix="/optimize", tags=["optimize"])
 
@@ -14,6 +15,26 @@ async def debug_payload(session_id: str):
         import json
         print(json.dumps(payload, indent=4))
         print("==========================\n")
-        return {"mensagem": "Payload gerado com sucesso! Veja o terminal.", "payload": payload}
+        
+        # Enviando para o n8n 
+        # TODO Trocar para URL de produção
+        WEB_HOOK_URL = "https://corvosaerodesign.app.n8n.cloud/webhook-test/a7550ac0-8b33-4076-8b47-bec3996cefd1"
+
+        response = requests.post(WEB_HOOK_URL, json=payload)
+        
+        # Verificando a resposta
+        n8n_data = 0
+        if response.status_code == 200:
+            print('Requisição enviada com sucesso!')
+            try:
+                n8n_data = response.json()
+            except ValueError:
+                ...
+        else:
+            print(f'Erro ao enviar: {response.status_code} - {response.text}')
+            n8n_data = {"erro": "Resposta não era JSON", "texto": response.text}
+
+        
+        return {"mensagem": "Payload gerado com sucesso! Veja o terminal.", "Retorno": n8n_data}
     except Exception as e:
         return {"erro": str(e)}
